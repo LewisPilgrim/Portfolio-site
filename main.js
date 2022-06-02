3994e332033d86d2c4c930c2e192bcc7efdce49a
Lewis Pilgrim <lewis.pilgrim0@hotmail.co.uk>
2022-06-02 15:34:15 +0100

    add popup windows and update images

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ function projectExpand() {
 }
 whatIMade.addEventListener('click', projectExpand);
 
-// Project minimize on outside click
+// Project minimize on button click
 function projectShrink() {
     if (projects.style.display === 'flex') {
         whatIMade.style.display = 'block';
@@ -35,3 +35,41 @@ function seeMoreGrow() {
     }
 }
 topButton.addEventListener('click', seeMoreGrow);
+
+// Popup images
+const popupOne = document.getElementById('first-project');
+const popupTwo = document.getElementById('second-project');
+const popupThree = document.getElementById('third-project');
+const project1 = document.getElementById('project-1');
+const project2 = document.getElementById('project-2');
+const project3 = document.getElementById('project-3');
+function firstPopup() {
+    if (popupOne.style.display = 'none') {
+        popupOne.style.display = 'block';
+    }
+}
+project1.addEventListener('click', firstPopup);
+function secondPopup() {
+    if (popupTwo.style.display = 'none') {
+        popupTwo.style.display = 'block';
+    }
+}
+project2.addEventListener('click', secondPopup);
+function thirdPopup() {
+    if (popupThree.style.display = 'none') {
+        popupThree.style.display = 'block';
+    }
+}
+project3.addEventListener('click', thirdPopup);
+const popupCloseButton = document.getElementsByClassName('popup-close');
+function popupClose() {
+    for (let i = 0; i < popupCloseButton.length; i++) {
+        popupCloseButton[i].addEventListener('click', function() {
+            if (popupOne.style.display === 'block' || popupTwo.style.display === 'block' || popupThree.style.display === 'block') {
+                popupOne.style.display = 'none';
+                popupTwo.style.display = 'none';
+                popupThree.style.display = 'none';
+          }});
+}
+}
+popupClose();
