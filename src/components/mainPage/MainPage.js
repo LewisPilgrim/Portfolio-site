b52f3719949a10f31e4964ca7963d4c7aa1707a4
LewisPilgrim <lewis.pilgrim0@hotmail.co.uk>
2023-03-18 18:08:54 +0000

    update to React portfolio app

---
+++ src/components/mainPage/MainPage.js
@@ -0,0 +1,60 @@
+import React from "react";
+import About from "./About";
+import ProjectList from "./projects/ProjectList";
+import "./MainPage.css";
+import SeeMoreBtn from "./SeeMoreBtn";
+import ScrollBtn from "./ScrollBtn";
+import { AiFillLinkedin, AiFillPhone } from "react-icons/ai";
+import { MdEmail } from "react-icons/md";
+
+const MainPage = () => {
+  const aboutRef = React.createRef();
+  const topRef = React.createRef();
+
+  const scrollToAbout = () => {
+    aboutRef.current?.scrollIntoView({ behavior: "smooth" });
+  };
+  const scrollToTop = () => {
+    topRef.current?.scrollIntoView({ behavior: "smooth" });
+  };
+
+  return (
+    <div className="page-window" ref={topRef}>
+      <div className="project-section">
+        <ProjectList />
+        <SeeMoreBtn scrollToAbout={scrollToAbout} />
+        <hr />
+      </div>
+      <div ref={aboutRef}></div>
+      <About />
+      <hr />
+      <div className="contact">
+        <h2>Contact me</h2>
+        <span>
+          <MdEmail color="#2f4f4f" size={19} title="Email" />
+          <h4>lewis.pilgrim0@hotmail.co.uk</h4>
+        </span>
+        <span>
+          <AiFillPhone color="#2f4f4f" size={19} title="Phone" />
+          <h4>07975 888183</h4>
+        </span>
+        <span>
+          <AiFillLinkedin color="#2f4f4f" size={19} title="LinkedIn" />
+          <h4>https://www.linkedin.com/in/lewis-pilgrim-73ab08236/</h4>
+        </span>
+      </div>
+      <ScrollBtn scrollToTop={scrollToTop} />
+      <footer>
+        <p>
+          Made by{" "}
+          <a href="https://www.linkedin.com/in/lewis-pilgrim-73ab08236/" title="Lewis' LinkedIn">
+            Lewis Pilgrim
+          </a>{" "}
+          2023
+        </p>
+      </footer>
+    </div>
+  );
+};
+
+export default MainPage;
---
+++ src/project-data.js
@@ -0,0 +1,37 @@
+export const projectsData = [
+    {
+        name: "Pokedex",
+        id: 1,
+        img: "./Images/pokedex-screenshot-top.jpg",
+        description: "An app built to show Pokemon and their stats when searched by either name or id. The search fetches data from the Pokedex API and returns the name, type and abilities of the relevant Pokemon as well as an image of it. Performance was a key aspect of this project and the site achieved a Lighthouse score of 99 on desktop and 86 on mobile.",
+        techUsed: "React, Redux, CSS, HTML, Pokemon API"
+    },
+    {
+        name: "Reddit Lite",
+        id: 2,
+        img: "./Images/reddit-lite-screenshot-wallpapers.jpg",
+        description: "An app which links to the Reddit API and displays the top posts from the subreddit that has been searched for. Upvotes of a post can be seen as well as how many comments there are on it. The links for 'See More' and 'Comments' will take the user to the relevant part of the post's page on the Reddit site. I tried to keep the visual appearance of the site as close to that of the official Reddit site as I could so that it would make sense to users.",
+        techUsed: "React, Redux, CSS, HTML, Reddit json API"
+    },
+    {
+        name: "Jammming",
+        id: 3,
+        img: "./Images/jammming-screenshot.jpg",
+        description: "As one of the projects on my Codecademy course i made a React application which allows users to make playlists for Spotify. This project made use of the Spotify API to allow users to log in, search for an artist or song on the Spotify database, add tracks to a custom playlist and then add that playlist to their Spotify account. There was a lot of work put into linking up the site with the Spotify API and allowing users to login.",
+        techUsed: "React, CSS, HTML, Spotify API"
+    },
+    {
+        name: "Login Form",
+        id: 4,
+        img: "./Images/login-form-screenshot.jpg",
+        description: "This was a small project from a Udemy course that I did which was to create a Login screen in React which allows users to move between different sections with animated transitions. There are three pages: Login, Register and Reset which can be moved between seamlessly using the links and buttons. There is a functioning password reveal button and a password strength helper on the Register screen.",
+        techUsed: "React, SASS, CSS, HTML"
+    },
+    {
+        name: "East Grinstead DnD",
+        id: 5,
+        img: "./Images/dnd-site-screenshot.jpg",
+        description: "One of my earlier projects, the aim was to create a site using HTML and CSS which is aesthetically pleasing and shows off my visual design skills.",
+        techUsed: "HTML, CSS"
+    }
+]
