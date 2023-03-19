0fe7b90302d92f1fe1fd83fba04e35e17ae01bbc
LewisPilgrim <lewis.pilgrim0@hotmail.co.uk>
2023-03-19 17:35:36 +0000

    image issue try fix

--- src/project-data.js
+++ src/project-data.js
@@ -1,8 +1,11 @@
+import image1 from './Images/pokedex-screenshot-top'
+
+
 export const projectsData = [
     {
         name: "Pokedex",
         id: 1,
-        img: "./Images/pokedex-screenshot-top.jpg",
+        img: image1,
         description: "An app built to show Pokemon and their stats when searched by either name or id. The search fetches data from the Pokedex API and returns the name, type and abilities of the relevant Pokemon as well as an image of it. Performance was a key aspect of this project and the site achieved a Lighthouse score of 99 on desktop and 86 on mobile.",
         techUsed: "React, Redux, CSS, HTML, Pokemon API"
     },
@@ -16,21 +19,21 @@ export const projectsData = [
     {
         name: "Jammming",
         id: 3,
-        img: "Images/jammming-screenshot.jpg",
+        img: "./lewispilgrim.github.io/Images/jammming-screenshot.jpg",
         description: "As one of the projects on my Codecademy course i made a React application which allows users to make playlists for Spotify. This project made use of the Spotify API to allow users to log in, search for an artist or song on the Spotify database, add tracks to a custom playlist and then add that playlist to their Spotify account. There was a lot of work put into linking up the site with the Spotify API and allowing users to login.",
         techUsed: "React, CSS, HTML, Spotify API"
     },
     {
         name: "Login Form",
         id: 4,
-        img: "./Images/login-form-screenshot.jpg",
+        img: "./lewispilgrim.github.io/Images/login-form-screenshot.jpg",
         description: "This was a small project from a Udemy course that I did which was to create a Login screen in React which allows users to move between different sections with animated transitions. There are three pages: Login, Register and Reset which can be moved between seamlessly using the links and buttons. There is a functioning password reveal button and a password strength helper on the Register screen.",
         techUsed: "React, SASS, CSS, HTML"
     },
     {
         name: "East Grinstead DnD",
         id: 5,
-        img: "./Images/dnd-site-screenshot.jpg",
+        img: "./lewispilgrim.github.io/Images/dnd-site-screenshot.jpg",
         description: "One of my earlier projects, the aim was to create a site using HTML and CSS which is aesthetically pleasing and shows off my visual design skills.",
         techUsed: "HTML, CSS"
     }
